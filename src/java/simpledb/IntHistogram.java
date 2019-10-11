220ca9675d6a47e9442c2c51b465f9dd456f42a9
julius <zjuxlh1993@gmail.com>
2019-10-11 17:57:03 +0800

    tmp

--- src/java/simpledb/IntHistogram.java
+++ src/java/simpledb/IntHistogram.java
@@ -1,9 +1,33 @@
 package simpledb;
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
 /** A class to represent a fixed-width histogram over a single integer-based field.
  */
 public class IntHistogram {
 
+	private class BucketInfo {
+		int sum, num;
+		void addValue(int v) {
+			sum += v;
+			++num;
+		}
+		
+		int getNum() {
+			return num;
+		}
+		
+		public BucketInfo() {
+			sum = 0;
+			num = 0;
+		}
+	}
+	
+	int min,max,totalNum,bs;
+	double interval;
+	
+	private BucketInfo[] buckets;
+	
     /**
      * Create a new IntHistogram.
      * 
@@ -22,14 +46,32 @@ public class IntHistogram {
      */
     public IntHistogram(int buckets, int min, int max) {
     	// some code goes here
+    	bs = buckets;
+    	this.buckets = new BucketInfo[buckets];
+    	for (int i=0;i!=buckets;i++) this.buckets[i]= new BucketInfo(); 
+    	this.min = min;
+    	this.max = max + 1;
+    	interval = (double)(this.max - this.min) / buckets;
+    	totalNum = 0;
     }
 
+    private double cal(int v) {
+    	if (v > max-1) return 0;
+    	double sum = 0;
+    	for (int i=(int)Math.floor((v-min)/interval); i!=bs; i++) {
+    		sum += buckets[i].getNum();
+    	}
+    	return sum;
+    }
+    
     /**
      * Add a value to the set of values that you are keeping a histogram of.
      * @param v Value to add to the histogram
      */
     public void addValue(int v) {
     	// some code goes here
+    	buckets[(int)Math.floor((v-min)/interval)].addValue(v);
+    	++totalNum;
     }
 
     /**
@@ -43,8 +85,24 @@ public class IntHistogram {
      * @return Predicted selectivity of this particular operator and value
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
-
+    	if (v < min) v = min;
     	// some code goes here
+    	switch (op) {
+		case EQUALS:
+			return (double)buckets[(int)Math.floor((v-min)/interval)].getNum()/interval/totalNum;
+		case NOT_EQUALS:
+			return 1.0 - (double)buckets[(int)Math.floor((v-min)/interval)].getNum()/interval/totalNum;
+		case GREATER_THAN:
+			return cal(v) / totalNum;
+		case GREATER_THAN_OR_EQ:
+			return cal(v) / totalNum;
+		case LESS_THAN:
+			return 1.0 - estimateSelectivity(op.GREATER_THAN_OR_EQ, v);
+		case LESS_THAN_OR_EQ:
+			return 1.0 - estimateSelectivity(op.LESS_THAN, v);
+		default:
+			break;
+		}
         return -1.0;
     }
     
